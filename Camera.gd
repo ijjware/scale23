828c767b933055846fe78ee2fd4402528317e50a
Nihlcorg <lukdiemic@gmail.com>
2023-11-10 01:03:57 -0500

    added mountain

--- Camera.gd
+++ Camera.gd
@@ -1,7 +1,7 @@
 extends Camera3D
 
 @onready var marker = $"../CharacterBody3D/followDAD/Marker3D"
-@onready var follow = $"../CharacterBody3D"
+@onready var follow = $"../CharacterBody3D/followDAD"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
@@ -9,4 +9,4 @@ func _ready():
 func _process(_delta):
 #		self.transform = marker.global_transform
 		self.transform = self.transform.interpolate_with(marker.global_transform, 0.07)
-		look_at(follow.position)
+		look_at(follow.global_position)
