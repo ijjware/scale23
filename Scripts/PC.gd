386ddfe5307338f8028e736c9853936ee6f68a00
Chris K <cl_killen@hotmail.com>
2023-11-12 18:14:54 -0500

    Camera and movement change

--- Scripts/PC.gd
+++ Scripts/PC.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
+const SPEED = 9.0
 const JUMP_VELOCITY = 4.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -25,13 +25,14 @@ func _physics_process(delta):
 	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir = Input.get_vector("ui_down", "ui_up", "ui_left", "ui_right", )
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	print(input_dir)
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+		rotate_y(deg_to_rad(input_dir.y * -2))
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
 	move_and_slide()
