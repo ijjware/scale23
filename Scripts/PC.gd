d576488d35f2cc2dede1f19c08bc9a920f5a0c31
Nihlcorg <lukdiemic@gmail.com>
2023-11-13 17:41:19 -0500

    added models, changed rotating
    added tree, cactus, spring, scale, cracked tile, and pig blend files. fixed character rotation mechanism, sped up movement variables(walking, jumping, falling). enabled auto-exposure to camera to add brightness to scenes(could be a better solution)

--- Scripts/PC.gd
+++ Scripts/PC.gd
@@ -1,18 +1,18 @@
 extends CharacterBody3D
 
 
-const SPEED = 9.0
-const JUMP_VELOCITY = 4.5
+const SPEED = 30
+const JUMP_VELOCITY = 80
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-@onready var cam = $"../Camera3D"
+@onready var cam = $Camera3D
 @onready var marker = $followDAD/Marker3D
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y -= gravity * delta
+		velocity.y -= gravity * delta *12
 	
 	$followDAD.transform = $followDAD.transform.rotated(Vector3(0,1,0), (.07*Input.get_last_mouse_velocity().normalized().x))
 	
@@ -22,15 +22,15 @@ func _physics_process(delta):
 #		print(Input.get_last_mouse_velocity())
 #	if Input.is_action_just_pressed("ui_accept"):
 #		pass
-	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("ui_down", "ui_up", "ui_left", "ui_right", )
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = (transform.basis * Vector3(input_dir.x, input_dir.y, 0)).normalized()
 	print(input_dir)
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+		transform = transform.rotated_local(Vector3(0,1,0), deg_to_rad(direction.y * -2))
 		rotate_y(deg_to_rad(input_dir.y * -2))
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
