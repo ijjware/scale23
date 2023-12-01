dddd512b9bd54d595ccd173a3237802fe26d0946
Nihlcorg <lukdiemic@gmail.com>
2023-12-01 06:42:03 -0500

    clear panel

--- Scripts/root.gd
+++ Scripts/root.gd
@@ -3,16 +3,9 @@ extends Node3D
 var level_time = 0.0
 @export var total_cakes = 8
 
-#manages animations and interactions between objects in scene
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	level_time += delta
-	pass
-
 
 func _on_hole_entered(body):
 	body.queue_free()
